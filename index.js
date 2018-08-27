ac9f99b5b3f4ec955923f2cb9333c5ff5af1e689
David Jacobs <djacobs@kungfulinux.com>
2018-08-27 12:45:12 -0400

    fixing button

--- index.js
+++ index.js
@@ -130,11 +130,8 @@ controller.hears("today in history", 'ambient', function(bot, message) {
 		var description = channel.item.description;
 		if (description.length > 140) {
 			var resultarray = description.split("</p>");
-			var result = resultarray[0] + '{ "name": "more", 
-				"text": "I Want to know more", 
-				"type": "button", 
-				"value": "https://www.history.com/this-day-in-history"
-		        }'
+		        var button = '{ "name": "link", "text": "I Want to know more", "type": "button", "value": "https://www.history.com/this-day-in-history" }'
+			var result = resultarray[0] + "/n" + button; 
 			bot.reply(message, title + striptags("```" + result + "```"));
 		} else {
 	       		bot.reply(message, title + striptags("```" + description + "```")); 
