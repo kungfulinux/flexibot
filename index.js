5836a8e9a4227b512e28c06704c74a5a4c60277a
kungfulinux <djacobs@kungfulinux.com>
2018-12-19 10:51:18 -0500

    added ) that was left out

--- index.js
+++ index.js
@@ -332,7 +332,7 @@ controller.hears(
                                    });
                                 });
 			} else {
-				bot.reply(message, "usage: flexibot whois <first_name>"
+				bot.reply(message, "usage: flexibot whois <first_name>")
                         });
 
 controller.hears(
