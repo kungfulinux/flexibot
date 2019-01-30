e6b35f47571fea81394afd8c1778f67e8dd868c7
Tim Plummer <tplummer@flexion.us>
2019-01-30 16:30:16 -0600

    fix message method

--- acronymicon/index.js
+++ acronymicon/index.js
@@ -1,6 +1,6 @@
 var acronymicon = require("./acronymicon.json");
 module.exports.default = function(bot, message) {
-  var acr = message.split(" ").pop();
+  var acr = message.text.split(" ").pop();
   var msg =
     acronymicon[acr] || "https://media.giphy.com/media/KOc9VDl9U06s0/giphy.gif";
   bot.reply(message, msg);
