849d6791709186b1a07fdc443f2a9781f88cde9b
Tim Plummer <tplummer@flexion.us>
2019-01-31 15:04:07 -0600

    integrate with google sheet

--- acronymicon/index.js
+++ acronymicon/index.js
@@ -1,7 +1,40 @@
-var acronymicon = require("./acronymicon.json");
-module.exports.default = function(bot, message) {
-  var acr = message.text.split(" ").pop();
-  var term = acronymicon[acr];
-  var msg = term ? term.title + ": " + term.definition : "https://media.giphy.com/media/KOc9VDl9U06s0/giphy.gif";
-  bot.reply(message, msg);
+//var acronymicon = require("./acronymicon.json");
+var fetch = require("node-fetch");
+var summonAcronymicon = function() {
+  var options = {
+    sheet: "1le17uVrLisQeylN7fsA2QvvP-FjBI63MPgULOaSRqxU",
+    key: "AIzaSyAN7P6J6xhT80OWrvSvFk1e1hZC8O_gzVM"
+  };
+  var url =
+    "https://sheets.googleapis.com/v4/spreadsheets/" +
+    options.sheet +
+    "/values/Sheet1!A1:C999?key=" +
+    options.key;
+  return fetch(url)
+    .then(response => response.json())
+    .then(json => {
+      var data = json.values ? json.values.slice(1) : [];
+      const normalize = data.reduce((acc, cur) => {
+        acc[cur[0]] = { title: cur[1], definition: cur[2] };
+        return acc;
+      }, {});
+      return normalize;
+    });
+};
+
+var runAcronymicon = function(bot, message) {
+  summonAcronymicon().then(acronymicon => {
+    var acr = message.text.split(" ").pop();
+    var term = acronymicon[acr];
+    var msg = term
+      ? term.title + ": " + term.definition
+      : "https://media.giphy.com/media/KOc9VDl9U06s0/giphy.gif";
+    console.log(msg);
+    //  bot.reply(message, msg);
+  });
+};
+
+module.exports = {
+  default: runAcronymicon,
+  summon: summonAcronymicon
 };
