d658415a9c9dea0727d7551dc07bcbd00dc81b48
Tim Plummer <tplummer@flexion.us>
2019-01-31 10:21:48 -0600

    add formatted terms

--- acronymicon/index.js
+++ acronymicon/index.js
@@ -1,7 +1,7 @@
 var acronymicon = require("./acronymicon.json");
 module.exports.default = function(bot, message) {
   var acr = message.text.split(" ").pop();
-  var msg =
-    acronymicon[acr] || "https://media.giphy.com/media/KOc9VDl9U06s0/giphy.gif";
+  var term = acronymicon[acr];
+  var msg = term ? term.title + ": " + term.definition : "https://media.giphy.com/media/KOc9VDl9U06s0/giphy.gif";
   bot.reply(message, msg);
 };
