eb2e8aec095ae81c7361b82f600cd69f7a3e53f0
Wes Dean <wdean@flexion.us>
2019-06-25 09:52:42 -0400

    Fix offhours path

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function pagerduty_offhours(line_separator) {
     line_separator = "\n";
   }
 
-  var offhours = require ('offhours/offhours.js');
+  var offhours = require ('./offhours/offhours.js');
   
   if (offhours.is_offhours (new Date())) {
     return pagerduty_message (line_separator);
