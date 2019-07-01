62f25c4c45fd4107381a4dcb868206bb06716ebc
Ubuntu <ubuntu@ip-172-31-43-168.ec2.internal>
2019-07-01 10:03:33 -0400

    added chris

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function shuffle(array) {
 
 function standup_list() {
   var arr = [
-    "tim", "wes", "ruth", "nic", "scott h", "charlie", "jon", "wilson", "aj", "john k", "jesse", "manoj", "clyde", "david", "andrew", "peter"
+    "tim", "wes", "ruth", "nic", "scott h", "charlie", "jon", "wilson", "aj", "chris", "john k", "jesse", "manoj", "clyde", "david", "andrew", "peter"
   ];
   arr = shuffle(arr);
   console.log(arr.join(","));
