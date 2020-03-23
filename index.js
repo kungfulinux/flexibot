e76257a8db12353ff3ef63be084ef31b99a33854
JUIM <djacobs@flexion.us>
2020-03-23 10:29:35 -0400

    troubleshooting

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function global_pandemic() {
     request('https://corona.lmao.ninja/all', function (error, response, body) {
     // console.error('error:', error); // Print the error if one occurred
     // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
+        console.log('body:', body); // Print the HTML for the Google homepage.
     
         const parseJsonAsync = (jsonString) => {
           return new Promise(resolve => {
