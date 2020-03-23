c186b2ed5f3af46b3b625911f4b9a4b0b0a08c38
JUIM <djacobs@flexion.us>
2020-03-23 10:39:32 -0400

    troubleshooting again

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var acronymicon = require("./acronymicon").default;
 var jira_matcher = require("./jira_matcher/jira_matcher.js");
 var offhours = require("./offhours/offhours.js");
-const request = require('request')
 
 function shuffle(array) {
   var currentIndex = array.length,
@@ -53,23 +52,25 @@ function onInstallation(bot, installer) {
 }
 
 function global_pandemic() {
+    const request = require('request')
     request('https://corona.lmao.ninja/all', function (error, response, body) {
-    // console.error('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
     
-        const parseJsonAsync = (jsonString) => {
-          return new Promise(resolve => {
-            setTimeout(() => {
-              resolve(JSON.parse(jsonString))
-            })
-          })
-        }
-        parseJsonAsync(body).then(jsonData => console.log(jsonData))
-        return(JSON.stringify(body))
+    const parseJsonAsync = (jsonString) => {
+      return new Promise(resolve => {
+        setTimeout(() => {
+          resolve(JSON.parse(jsonString))
+        })
+      })
+    }
     
-    })
-  }
+    parseJsonAsync(body).then(jsonData => console.log(jsonData))
+    return(JSON.stringify(body))
+
+  }) 
+}
 
 
 
