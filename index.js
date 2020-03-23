41eaca98994ad66843514ae76f675ad025edf4e7
JUIM <djacobs@flexion.us>
2020-03-23 13:10:53 -0400

    fixed typo

--- index.js
+++ index.js
@@ -210,25 +210,25 @@ controller.hears(
 );
 
 controller.hears(["global pandemic"], ["ambient"], function(bot, message) {
-  const request = require('request')
-  request('https://corona.lmao.ninja/all', function (error, response, body) {
-  //console.error('error:', error); // Print the error if one occurred
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log('body:', body); // Print the HTML for the Google homepage.
-  
-  const parseJsonAsync = (jsonString) => {
-    return new Promise(resolve => {
-      setTimeout(() => {
-        resolve(JSON.parse(jsonString))
-      })
-    })
-  }
-  
-  parseJsonAsync(body).then(jsonData => console.log(jsonData))
-  //console.log(JSON.stringify(body))
-  bot.reply(message, JSON.stringify(body));
-});
-
+      const request = require('request')
+      request('https://corona.lmao.ninja/all', function (error, response, body) {
+      //console.error('error:', error); // Print the error if one occurred
+      //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      //console.log('body:', body); // Print the HTML for the Google homepage.
+      
+      const parseJsonAsync = (jsonString) => {
+        return new Promise(resolve => {
+          setTimeout(() => {
+            resolve(JSON.parse(jsonString))
+          })
+        })
+      }
+      
+      parseJsonAsync(body).then(jsonData => console.log(jsonData))
+      //console.log(JSON.stringify(body))
+      bot.reply(message, JSON.stringify(body));
+    });
+  });
 
 controller.hears(
   ["flexibot weather"],
